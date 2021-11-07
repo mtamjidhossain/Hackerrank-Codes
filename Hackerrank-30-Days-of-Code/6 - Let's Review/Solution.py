0ad051b4bb99b94cf3ba837d1ce5aa2141a3c529
Md Tamjid Hossain <72858423+mtamjidhossain@users.noreply.github.com>
2021-11-08 04:16:45 +0600

    Update Solution.py

--- Hackerrank-30-Days-of-Code/6 - Let's Review/Solution.py
+++ Hackerrank-30-Days-of-Code/6 - Let's Review/Solution.py
@@ -1,1 +1,17 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
 
+NumOfLines = int(input())               # taking first input 2
+
+
+for j in range(NumOfLines):             # makes sure we take all the lines as n
+    
+    s = input()                         # taking 1st and 2nd line
+    evens, odds = '',''
+
+    for i in range(len(s)):
+        if i % 2 == 0:
+            evens += s[i]
+        else:
+            odds += s[i]
+
+    print(evens, odds)  
