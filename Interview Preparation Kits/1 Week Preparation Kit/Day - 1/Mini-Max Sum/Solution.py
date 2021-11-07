4f0b7ca6ca11a5a70156f11482ae324ef723530c
Md Tamjid Hossain <72858423+mtamjidhossain@users.noreply.github.com>
2021-11-08 03:22:25 +0600

    Update Solution.py

--- Interview Preparation Kits/1 Week Preparation Kit/Day - 1/Mini-Max Sum/Solution.py
+++ Interview Preparation Kits/1 Week Preparation Kit/Day - 1/Mini-Max Sum/Solution.py
@@ -1,1 +1,34 @@
+#!/bin/python3
 
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    # Write your code here
+
+    sums = []
+
+    for i in range(len(arr)):         # ignoring i in the iteration
+        itr_sum = 0                         # sum for each iteration  
+        for j in range(len(arr)):     # Iterations: j traversing from first to last element                 
+            if i != j:                # if 1 != 1 then s += 2,3,4,5
+                itr_sum += arr[j]
+                
+        sums.append(itr_sum)
+
+    print(min(sums), max(sums))
+    
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
