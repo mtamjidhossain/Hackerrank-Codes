cb215af4aa828cdce3e237a15f1ea97be42129bb
Md Tamjid Hossain <72858423+mtamjidhossain@users.noreply.github.com>
2021-11-08 00:12:25 +0600

    Update Solution.py

--- Interview Preparation Kits/1 Week Preparation Kit/Day 1 - Plus Minus/Solution.py
+++ Interview Preparation Kits/1 Week Preparation Kit/Day 1 - Plus Minus/Solution.py
@@ -1,1 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def plusMinus(arr):
+    # Write your code here
+    pos, neg, zer = 0,0,0
+    for i in arr:
+        if   i > 0: pos += 1
+        elif i < 0: neg += 1
+        elif i ==0: zer += 1
+    print("{:.6f}\n{:.6f}\n{:.6f}".format(pos/n,neg/n,zer/n))
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
 
