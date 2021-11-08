265ebda8fb88fe6be2159b6694d37df45d7b4cb2
Md Tamjid Hossain <72858423+mtamjidhossain@users.noreply.github.com>
2021-11-09 03:59:22 +0600

    Update Solution.py

--- Hackerrank-30-Days-of-Code/8 - Dictionaries and Maps/Solution.py
+++ Hackerrank-30-Days-of-Code/8 - Dictionaries and Maps/Solution.py
@@ -1,1 +1,18 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+my_dict = dict()
 
+for i in range(n):
+    x = input().split()
+    my_dict[x[0]] = x[1]
+
+while True:
+    try:
+        key = input()
+        
+        if key in my_dict:
+            print(key, '=', my_dict[key], sep='')
+        else:
+            print('Not found')
+    except:
+        break
