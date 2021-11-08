a76446889cc0538cf7a7f6b0f1bf1caa9afd3508
Md Tamjid Hossain <72858423+mtamjidhossain@users.noreply.github.com>
2021-11-08 23:30:38 +0600

    Update Solution.py

--- Hackerrank-30-Days-of-Code/7 - Arrays/Solution.py
+++ Hackerrank-30-Days-of-Code/7 - Arrays/Solution.py
@@ -1,1 +1,19 @@
+#!/bin/python3
 
+import math
+import os
+import random
+import re
+import sys
+
+def reverse_array(arr):
+    for i in range(len(arr)):
+        print(arr[len(arr) - 1 - i], end= " ")
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+    
+    reverse_array(arr)
