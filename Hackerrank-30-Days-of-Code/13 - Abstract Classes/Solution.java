859e17ef4032fa3dd9203e74177c6fcd0e05fedb
Md Tamjid Hossain <72858423+mtamjidhossain@users.noreply.github.com>
2021-11-12 05:24:55 +0600

    Update Solution.java

--- Hackerrank-30-Days-of-Code/13 - Abstract Classes/Solution.java
+++ Hackerrank-30-Days-of-Code/13 - Abstract Classes/Solution.java
@@ -12,29 +12,8 @@ abstract class Book {
     abstract void display();
 }
 
-// Declare your class here. Do not use the 'public' access modifier.
-    // Declare the price instance variable
-    
-    /**   
-    *   Class Constructor
-    *   
-    *   @param title The book's title.
-    *   @param author The book's author.
-    *   @param price The book's price.
-    **/
-    // Write your constructor here
-    
-    /**   
-    *   Method Name: display
-    *   
-    *   Print the title, author, and price in the specified format.
-    **/
-    // Write your method here
-    
-// End class
 class MyBook extends Book{
     int price;
-
     MyBook(String title, String author, int price){
         super(title, author);
         this.price = price;
